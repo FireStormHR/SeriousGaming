1a0be49d5b5e00fb38a3caccc3211188eabf7cd2
SG1995 <0898958@hr.nl>
2017-02-16 09:33:02 +0100

    Refactor

--- TetrisCalculatingGame/TetrisCalculatingGame/Game1.cs
+++ TetrisCalculatingGame/TetrisCalculatingGame/Game1.cs
@@ -1,24 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-
-
 namespace TetrisCalculatingGame
 {
-
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        KeyboardState oldState;             //Makes it possible to compare the old keyboard state with the new one
-        MouseState oldMouse;                //Makes it possible to compare the old mouse state with the new one
-        SpriteBatch spriteBatch;
-        Dictionary<string, Texture2D> All_Textures = new Dictionary<string, Texture2D>();
-        SpriteFont Arial_12 = null;
+        private Dictionary<string, Texture2D> All_Textures = new Dictionary<string, Texture2D>();
+        private SpriteFont Arial_12;
+        private readonly GraphicsDeviceManager graphics;
+        private MouseState oldMouse; //Makes it possible to compare the old mouse state with the new one
+        private KeyboardState oldState; //Makes it possible to compare the old keyboard state with the new one
+        private SpriteBatch spriteBatch;
 
 
         public Game1()
@@ -34,16 +28,18 @@ namespace TetrisCalculatingGame
             base.Initialize();
             oldState = Keyboard.GetState();
             oldMouse = Mouse.GetState();
-            this.IsMouseVisible = true;     //You can see the cursor
-            this.graphics.IsFullScreen = true;
-            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height; //Set xna resolution height to curr screen resolution
-            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;   //Set xna resolution width to curr screen resolution
+            IsMouseVisible = true; //You can see the cursor
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+                //Set xna resolution height to curr screen resolution
+            graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                //Set xna resolution width to curr screen resolution
             graphics.ApplyChanges();
         }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        ///     LoadContent will be called once per game and is the place to load
+        ///     all of your content.
         /// </summary>
         protected override void LoadContent()
         {
@@ -51,16 +47,15 @@ namespace TetrisCalculatingGame
             spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
 
 
-
             Arial_12 = Content.Load<SpriteFont>("NewSpriteFont");
-            
+
 
             // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
+        ///     UnloadContent will be called once per game and is the place to unload
+        ///     game-specific content.
         /// </summary>
         protected override void UnloadContent()
         {
@@ -69,13 +64,14 @@ namespace TetrisCalculatingGame
         }
 
         /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
+        ///     Allows the game to run logic such as updating the world,
+        ///     checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
@@ -84,7 +80,7 @@ namespace TetrisCalculatingGame
         }
 
         /// <summary>
-        /// This is called when the game should draw itself.
+        ///     This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
